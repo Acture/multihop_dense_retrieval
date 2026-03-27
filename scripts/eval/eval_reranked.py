4872b392203d5731254e0e3be774d8837bb40caf
Acture <acturea@gmail.com>
2026-03-27 12:59:59 +0800

    Clean up MDR formatting and docstrings

--- scripts/eval/eval_reranked.py
+++ scripts/eval/eval_reranked.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # All rights reserved.
 #
-# This source code is licensed under the license found in the 
+# This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 import json
 import numpy as np
@@ -11,13 +11,14 @@ from tqdm import tqdm
 
 
 corpus = json.load(open("../data/hotpot_index/wiki_id2doc.json"))
-title2text = {v["title"]:v["text"] for v in corpus.values()}
+title2text = {v["title"]: v["text"] for v in corpus.values()}
 
-val_inputs = [json.loads(l) for l in open("../data/hotpot/hotpot_qas_val.json").readlines()]
-id2goldsp = {_["_id"]:_["sp"] for _ in val_inputs}
-id2goldans = {_["_id"]:_["answer"] for _ in val_inputs}
-id2type = {_["_id"]:_["type"] for _ in val_inputs}
-
+val_inputs = [
+	json.loads(l) for l in open("../data/hotpot/hotpot_qas_val.json").readlines()
+]
+id2goldsp = {_["_id"]: _["sp"] for _ in val_inputs}
+id2goldans = {_["_id"]: _["answer"] for _ in val_inputs}
+id2type = {_["_id"]: _["type"] for _ in val_inputs}
 
 
 simple_tokenizer = SimpleTokenizer()
@@ -39,25 +40,25 @@ ans_recalls = []
 bridge_ems = []
 compare_ems = []
 for qid in tqdm(results["titles"].keys()):
-    chain = results["titles"][qid]
-    sp = id2goldsp[qid]
-    answer = id2goldans[qid]
-    type_ = id2type[qid]
+	chain = results["titles"][qid]
+	sp = id2goldsp[qid]
+	answer = id2goldans[qid]
+	type_ = id2type[qid]
 
-    # if answer[0].strip() in ["yes", "no"]:
-    #     continue
+	# if answer[0].strip() in ["yes", "no"]:
+	#     continue
 
-    sp_covered = int(np.sum([int(_ in chain) for _ in sp]) == len(sp))
-    concat_p = "yes no " + " ".join([t + " " + title2text.get(t, "") for t in chain])
-    ans_covered = para_has_answer(answer, concat_p, simple_tokenizer)
-    ans_recalls.append(ans_covered)
-    sp_ems.append(sp_covered)
+	sp_covered = int(np.sum([int(_ in chain) for _ in sp]) == len(sp))
+	concat_p = "yes no " + " ".join([t + " " + title2text.get(t, "") for t in chain])
+	ans_covered = para_has_answer(answer, concat_p, simple_tokenizer)
+	ans_recalls.append(ans_covered)
+	sp_ems.append(sp_covered)
 
-    if type_ == "bridge":
-        bridge_ems.append(sp_covered)
-    else:
-        compare_ems.append(sp_covered)
-    
+	if type_ == "bridge":
+		bridge_ems.append(sp_covered)
+	else:
+		compare_ems.append(sp_covered)
+
 
 print(len(sp_ems))
 print(np.mean(sp_ems))
--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ from setuptools import setup, find_packages
 import sys
 import subprocess
 
-with open('README.md') as f:
-    readme = f.read()
+with open("README.md") as f:
+	readme = f.read()
 
 # with open('LICENSE') as f:
 #     license = f.read()
 
-with open('requirements.txt') as f:
-    reqs = f.read()
+with open("requirements.txt") as f:
+	reqs = f.read()
 
 setup(
-    name='mdr',
-    version='0.0.1',
-    description='Multi-hop dense retrieval for complex open-domain question answering',
-    long_description='text/markdown',
-    # license=license,
-    python_requires='>=3.6',
-    packages=find_packages(exclude=('data')),
-    install_requires=reqs.strip().split('\n'),
+	name="mdr",
+	version="0.0.1",
+	description="Multi-hop dense retrieval for complex open-domain question answering",
+	long_description="text/markdown",
+	# license=license,
+	python_requires=">=3.6",
+	packages=find_packages(exclude=("data")),
+	install_requires=reqs.strip().split("\n"),
 )
